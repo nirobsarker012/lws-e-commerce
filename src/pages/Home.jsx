3ccc62fdfba4b0eb3bff4d7280b036dcdaa9c16f
Nirob Sarker <nirobsarker012@gmail.com>
2025-08-24 12:26:08 +0600

    add: my-card-details

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Header from "../components/Header";
+import CardList from "../components/Card/CardList";
+import MyCardList from "../components/Card/MyCardList";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <main className="container mx-auto py-8">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+          <CardList />
+          <MyCardList />
+        </div>
+      </main>
     </>
   );
 };
